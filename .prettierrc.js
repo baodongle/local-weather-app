7f607263333653861b341800df80fe0bff9473b9
baodongle <baodongle@gmail.com>
2019-12-03 17:58:35 +0700

    Init project

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  printWidth: 120,
+  singleQuote: true,
+  useTabs: false,
+  tabWidth: 2,
+  semi: true,
+  bracketSpacing: true,
+  'object-curly-spacing': true,
+  'array-bracket-spacing': [true, 'never'],
+  trailingComma: 'es5',
+  parser: 'typescript',
+};
